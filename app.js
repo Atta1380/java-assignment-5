2f8d451cd291c774d9091765cce502db02e4a303
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-02-14 20:48:18 +0500

    added a javascript and html files

---
+++ app.js
@@ -0,0 +1,54 @@
+//--------Q1.
+
+//var studentNames =[];
+//console.log(studentNames)
+
+//--------Q2.
+
+//var studentNames =new Array();
+//console.log(studentNames)
+
+//--------Q3.
+
+//var stringarray =["cat","cow","mouse"];
+//console.log(stringarray)
+
+//--------Q4.
+
+//var numarray =["1","2","3"];
+//console.log(numarray)
+
+
+//--------Q5.
+
+//var booleanarray =["",""];
+//console.log(booleanarray)
+
+//--------Q6.
+
+//var mixarray =["cat",3];
+//console.log(mixarray)
+
+//--------Q7.
+
+//var edu =["SSC</br>","HSC</br>","BCS</br>","BS</br>", "BCOM</br>", "MS</br>", "Phil</br>", "PhD"];
+//console.log(edu)
+//document.write(edu)
+
+//--------Q8.
+
+//var studentNames =["Micheal","john","tony"];
+//var score =["320","230"."480"];
+//var percentages= 500
+
+
+//--------Q9.
+//--------a.
+
+//var colors =["</br>green","blue","</br>red</br>"];
+//document.write("colorsArray:"+colors+"</br>")
+
+//var addcolor= prompt("add that color to the beginning of the array.");
+//colors.unshift(addcolor);
+//document.write("Display the updated array in your browser:  "+" "+colors)
+
